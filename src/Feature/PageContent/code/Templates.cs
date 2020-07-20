eef0d6aa232eadb8e51a5e04ca816e932138d0bd
Dillon Coffman <dcoffman@ceiamerica.com>
2020-07-20 16:56:39 -0400

    Configured referring to Fields by ID instead of their name

--- src/Feature/PageContent/code/Templates.cs
+++ src/Feature/PageContent/code/Templates.cs
@@ -6,7 +6,14 @@ using Sitecore.Data;
 
 namespace Sitecon.Feature.PageContent
 {
-  public struct Templates
+  public static class Templates
   {
+    public static class BodyCopy
+    {
+      public static class Fields
+      {
+        public static readonly ID BodyCopy = new ID("{3CD357B9-95BF-4F3B-8E4F-FE717732F9A1}");
+      }
+    } 
   }
 }
---
+++ src/Foundation/SitecoreHelperExtensions/code/SitecoreHelperExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Sitecore.Data;
+using Sitecore.Mvc.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Foundation.SitecoreHelperExtensions
+{
+    public static class SitecoreHelperExtensions
+    {
+        public static HtmlString Field(this SitecoreHelper helper, ID fieldID)
+        {
+          return helper.Field(fieldID.ToString());
+        }
+    }
+}
