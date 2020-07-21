46e679dea1dc95604e2146619574232d8426937b
Dillon Coffman <dcoffman@ceiamerica.com>
2020-07-21 14:32:31 -0400

    Created TwoImage Rendering, data template, and added the data template as a base template on the EventDetail Page Type template

--- src/Feature/PageContent/code/Templates.cs
+++ src/Feature/PageContent/code/Templates.cs
@@ -14,6 +14,14 @@ namespace Sitecon.Feature.PageContent
       {
         public static readonly ID BodyCopy = new ID("{3CD357B9-95BF-4F3B-8E4F-FE717732F9A1}");
       }
-    } 
+    }
+    public static class TwoImage
+    {
+      public static class Fields
+      {
+        public static readonly ID Image1 = new ID("{DF162CB7-9DF1-455D-A6D5-FE9A05CB933C}");
+        public static readonly ID Image2 = new ID("{1333AB98-25E5-4C16-B15B-C0CB992642B1}");
+      }
+    }
   }
 }
--- src/Foundation/SitecoreHelperExtensions/code/SitecoreHelperExtensions.cs
+++ src/Foundation/SitecoreHelperExtensions/code/SitecoreHelperExtensions.cs
@@ -1,13 +1,8 @@
 ﻿using Sitecore.Data;
 using Sitecore.Mvc.Helpers;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Web;
 
-namespace Foundation.SitecoreHelperExtensions
+namespace Sitecon.Foundation.SitecoreHelperExtensions
 {
     public static class SitecoreHelperExtensions
     {
@@ -15,5 +10,11 @@ namespace Foundation.SitecoreHelperExtensions
         {
           return helper.Field(fieldID.ToString());
         }
-    }
+        
+        
+        public static HtmlString Field(this SitecoreHelper helper, ID fieldID, object parameters)
+        {
+          return helper.Field(fieldID.ToString(), parameters);
+        }
+  }
 }
